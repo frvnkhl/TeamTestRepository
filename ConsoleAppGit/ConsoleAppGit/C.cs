4dad99479a5f74bfb022f8ec0bc75649f7195787
jitkams <jitka.m.s@seynam.cz>
2021-08-31 20:34:04 +0200

    c1

---
+++ ConsoleAppGit/ConsoleAppGit/C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit.C
+{
+    class C
+    {
+
+        public void Cheer()
+        {
+            Console.WriteLine("Team name");
+        }
+    }
+
+
+}
