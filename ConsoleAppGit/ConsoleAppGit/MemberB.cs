db917eda724722e8d8e153ea008f71729b1f0f2f
charamzic <charamza@gmail.com>
2021-08-31 21:08:55 +0200

    b5

--- ConsoleAppGit/ConsoleAppGit/MemberB.cs
+++ ConsoleAppGit/ConsoleAppGit/MemberB.cs
@@ -4,9 +4,9 @@ namespace ConsoleAppGit
 {
     public class MemberB
     {
-        public static void Count(string paramToPrint)
+        public static void Count(string paramToPrint, string anotherParamToPrint)
         {
-            Console.WriteLine($"Jenicek and {paramToPrint}");
+            Console.WriteLine($"Jenicek and {paramToPrint} and also {anotherParamToPrint}");
         }
     }
 }
